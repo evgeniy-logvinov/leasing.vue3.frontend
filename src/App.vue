d19a9d2a1e55e69bbf7c166d6d8ce0dbe18b078c
Evgeniy Logvinov <evgeniy.logvinov.k@gmail.com>
2022-02-09 16:06:22 +0500

    i18n translation for element ui

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,20 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import { ElConfigProvider } from 'element-plus'
+import zhCn from 'element-plus/lib/locale/lang/zh-cn'
+
+const zIndex = 3000;
+const locale = zhCn;
+const size: "default" | "small" | "large" = "small";
+
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <el-config-provider :zIndex="zIndex" :locale="locale">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  </el-config-provider>
 </template>
 
 <style>
